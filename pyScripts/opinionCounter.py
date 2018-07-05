f817ec6f81c76969db99c2bb2705937604b5cd59
Harsh Narang <harshnarang8@gmail.com>
2018-07-05 10:00:56 -0500

    restructure repository for python script folder

---
+++ pyScripts/opinionCounter.py
@@ -0,0 +1,56 @@
+import xml.etree.ElementTree as ET
+import sys
+file = sys.argv[1]
+tree = ET.parse(file)
+
+root = tree.getroot()
+
+mySentences = root[0][0]
+
+pCategoryDict = {}
+nCategoryDict = {}
+cCategoryDict = {}
+
+sum3 = 0
+for review in root:
+    mySentences = (review.findall('sentences'))[0]
+    for sentence in mySentences:
+        text = sentence[0]
+        opinions = sentence[1]
+        for opinion in opinions:
+            if (not (pCategoryDict.has_key(opinion.attrib['category']))):
+                pCategoryDict[opinion.attrib['category']] = 0
+                nCategoryDict[opinion.attrib['category']] = 0
+                cCategoryDict[opinion.attrib['category']] = 0
+            if (opinion.attrib['implicit'] == "true"):
+                pCategoryDict[opinion.attrib['category']] += 1
+            elif (opinion.attrib['implicit'] == "false"):
+                nCategoryDict[opinion.attrib['category']] += 1
+            else:
+                cCategoryDict[opinion.attrib['category']] += 1
+
+sum1 = 0
+sum2 = 0
+
+print "Counts: "
+print sum(pCategoryDict.values())
+print sum(nCategoryDict.values())
+print sum(cCategoryDict.values())
+
+print "Implicit:"
+
+for key, val in pCategoryDict.iteritems():
+    if val != 0:
+        print str(key)+" : " + str(val)
+
+print "\nExplicit:"
+
+for key, val in nCategoryDict.iteritems():
+    if val != 0:
+        print str(key)+" : " + str(val)
+
+print "\nCoreference:"
+
+for key, val in cCategoryDict.iteritems():
+    if val != 0:
+        print str(key)+" : " + str(val)
